7a39f343232f9283a34e8e77ec0748bab7039511
Flasf <abdielr507@gmail.com>
2022-05-27 13:30:05 -0500

    Added random number generator also made the task json for GCC and CLANG compilers with terminal

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+//Librerías para el random number generator
+#include <cstdlib> //srand y rand
+#include <ctime> //time
+
+using std::cin; using std::cout;
+using std::endl; using std::string;
+
 int main()
 {
-    
+    srand((unsigned)time(NULL));
+    int iRandomNumber = rand()%100+1;
+    cout << iRandomNumber << endl;
 }
